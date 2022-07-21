9a5681e89047178bcb6fba5c659c7295b77ba8b1
sibaamap <nguyenhuyanh2510@gmail.com>
2022-07-21 11:52:12 +0700

    Button Navigation View

---
+++ app/src/main/java/com/sibaamap/bottomnavigationview/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.sibaamap.bottomnavigationview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.fistFragment, R.id.secondFragment, R.id.thirdFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration  )
+
+        buttonNavigationView.setupWithNavController(navController)
+    }
+}
